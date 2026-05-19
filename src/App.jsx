5757d89b69019b66a2b6391acdb6a94d85eafe3e
Fadlan <fadlan@example.com>
2026-05-19 20:02:00 +0700

    Update webhook URL in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
   
   const userProfile = { id: "user_fadlan_01", name: "Fadlan", jurusan: "RPL" };
   const chatContainerRef = useRef(null);
-  const N8N_WEBHOOK_URL = "https://attire-uncurled-endorphin.ngrok-free.dev/webhook/cognistra";
+  const N8N_WEBHOOK_URL = " https://producer-omissions-tomorrow-constructed.trycloudflare.com";
 
   const [progress, setProgress] = useState({
     fisika: { label: 'Fisika', level: 0, color: 'bg-blue-500', icon: <Atom size={18} className="text-blue-500"/> },
